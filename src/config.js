77e99c2586992b7367f227fa75de20ffb3312372
Mark Force <force.j.mark@gmail.com>
2020-11-06 14:12:35 -0500

    attempt #4

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export default {
-    API_ENDPOINT: process.env.API_ENDPOINT || "https://petful-api-server.herokuapp.com"
+    API_ENDPOINT: process.env.API_ENDPOINT || "https://petful-api-server.herokuapp.com/api"
 }
